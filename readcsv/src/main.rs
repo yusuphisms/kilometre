69ac02ceda9d168b5c2ef73173ec844e5dfe1be5
Yusuph Mkangara <yusuph.mka@outlook.com>
2022-10-10 23:57:31 -0700

    Resolved off by one error but want to reset position after each column return

--- readcsv/src/main.rs
+++ readcsv/src/main.rs
@@ -12,10 +12,8 @@ fn simple_reading_from_stdin() -> Result<(), Box<dyn Error>> {
     let mut v: Vec<u8> = Vec::new();
     io::stdin().read_to_end(&mut v)?;
     let mut reader = ReaderBuilder::new().from_reader(io::Cursor::new(v));
-    reader.records().next(); // skip the header row
-    let og_record_pos = reader.position().clone(); // get the position
-    println!("{:?}", og_record_pos);
-    reader.seek(csv::Position::new())?; // seek back to the beginning
+    reader.headers()?; // skip the header row by reading it first
+    let og_record_pos = reader.position().clone(); // get the position; this is the correct first record after headers
 
     // Reading by records
 
@@ -25,57 +23,19 @@ fn simple_reading_from_stdin() -> Result<(), Box<dyn Error>> {
     println!("{:?}", record);
     println!("{:?}", record.len());
 
-    // for result in reader.records() {
-    //     let record = result?;
-    //     println!("{:?}", record.position());
-    //     println!("{:?}", record);
-    //     println!("{:?}", record.len());
-    // }
-
     // Reading by fields
-    let test_pos = csv::Position::new().set_record(1).to_owned();
-    reader.seek(test_pos)?;
+    reader.seek(og_record_pos.clone())?;
     let mut r_n_f = RecordsAndFields::new(&mut reader);
     let field_record = r_n_f.field_next().unwrap()?;
     println!("{:?}", field_record);
-    println!("{:?}", field_record.len()); // We're closer but we are getting an off by one error, 1 record is being dropped for some reason.
+    println!("{:?}", field_record.len());
 
-    r_n_f.reader.seek(og_record_pos)?;
+    r_n_f.reader.seek(og_record_pos)?; // Ok thi is good -- NEXT: Let's have field_next internally reset the position
     let next_field_record = r_n_f.field_next().unwrap()?;
     println!("{:?}", next_field_record);
-    println!("{:?}", next_field_record.len()); // We're closer but we are getting an off by one error, 1 record is being dropped for some reason.
+    println!("{:?}", next_field_record.len());
 
-    // Problem! The reader can only be read once, so we can't iterate to get the next field if we have to read all records to read the first one...
-    // Looks like there is a Seek implementation, so this might just have been a limitation of using io::stdin() directly.
-    // I switched to using a Vec<u8> buffer and io::Cursor
-
-    // Back to basics:
-    // test_flatten(); // flatten can work, and is getting closer to a solution like ndarray
-
-    // let all_records: Vec<Result<StringRecord, csv::Error>> = reader.records().collect();
-    // let unwrapped_records: Vec<StringRecord> = all_records
-    //     .iter()
-    //     .map(|r| {
-    //         if r.is_err() {
-    //             StringRecord::new()
-    //         } else {
-    //             r.as_ref().ok().unwrap().to_owned()
-    //         }
-    //     })
-    //     .collect();
-    // let col1: Vec<&str> = unwrapped_records
-    //     .iter()
-    //     .map(|x| x.get(0).unwrap())
-    //     .collect();
-    // println!("{:?}", col1);
-
-    // let col2: Vec<&str> = unwrapped_records
-    //     .iter()
-    //     .map(|x| x.get(3).unwrap())
-    //     .collect();
-    // println!("{:?}", col2);
-
-    println!("{:?}", reader.headers());
+    println!("{:?}", reader.headers()?.position());
     Ok(())
 }
 
@@ -84,7 +44,7 @@ struct RecordsAndFields<'i, R: 'i> {
     current_field_iteration: usize,
 }
 
-impl<'i, R: io::Read> RecordsAndFields<'i, R> {
+impl<'i, R: io::Read + io::Seek> RecordsAndFields<'i, R> {
     fn new(reader: &'i mut Reader<R>) -> RecordsAndFields<'i, R> {
         Self {
             reader,
@@ -102,6 +62,7 @@ impl<'i, R: io::Read> RecordsAndFields<'i, R> {
         // Apparently I really need to review Iter / IntoIterator trait implementation, like bad.
         let pos = self.current_field_iteration;
         self.current_field_iteration += 1;
+        let starting_reader_position = self.reader.position().clone();
         for result in self {
             let record = result.ok()?;
             stringfield.push_field(record.get(pos)?);
@@ -132,9 +93,3 @@ impl<'i, R: io::Read> Iterator for RecordsAndFields<'i, R> {
 //         todo!()
 //     }
 // }
-
-fn test_flatten() {
-    let v = vec![vec!["1", "2", "3"], vec!["4", "5", "6"]];
-    let rez: Vec<Option<&&str>> = v.iter().map(|x| x.get(0)).collect();
-    println!("{:?}", rez);
-}
