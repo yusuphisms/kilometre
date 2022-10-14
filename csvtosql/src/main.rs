4cbfd6ae66a47c376b65c1670765acedd0c3299e
Yusuph Mkangara <yusuph.mka@outlook.com>
2022-10-14 00:18:24 -0700

    We have queries and magic

--- csvtosql/src/main.rs
+++ csvtosql/src/main.rs
@@ -1,19 +1,46 @@
-use rusqlite::vtab::csvtab;
-use rusqlite::{Connection, Result};
+use anyhow::Result;
+use csv::ReaderBuilder;
+use rusqlite::Connection;
+use std::io::{self, Read};
 
 fn main() -> Result<()> {
     // We are gathered here today, to import a CSV to Sqlite
-    // Right off the bat, we have two options we can consider: virtual table or not.
-    // The Virtual Table route: https://www.sqlite.org/csv.html
+    // When importing to SQL for the first time, knowing headers ahead of time is important.
+    // Since reading CSVs is tricky, using the csv crate to read and interpret headers vs rows is the right thing to do.
+
     let conn = Connection::open_in_memory()?;
-    csvtab::load_module(&conn)?;
-    let schema = "
-        CREATE VIRTUAL TABLE my_csv_data
-        USING csv(filename = 'testfile.csv')
-    ";
-    // the specs in the link above say that data is an available keyword where you can just put direct text in, but rusqlite's csvtab does not support that yet.
-    // And I can't figure out what the correct way is to tell it the path of the filename. Kinda crazy.
-    conn.execute_batch(schema)?;
+    // let mut bytes: Vec<u8> = Vec::new();
+    // io::stdin().read_to_end(&mut bytes)?;
+    let mut csv_reader = ReaderBuilder::new().from_reader(io::stdin());
+    let cloned = csv_reader.headers().cloned()?;
+    let headers: Vec<&str> = cloned.iter().collect();
+
+    let create_table = format!(
+        r#"
+        CREATE TABLE testing ({})
+        "#,
+        headers.join(",")
+    );
+
+    conn.execute(create_table.as_str(), ())?;
+
+    let mut stmt = conn.prepare("SELECT * FROM testing")?;
+    let mut rows = stmt.query([])?;
+
+    let prepared_insert = r#"
+    INSERT INTO testing ( col1 ) VALUES ( 1 )
+    "#;
+
+    let mut prepared_insert = conn.prepare_cached(prepared_insert)?;
+    let row = csv_reader.records().next().unwrap()?;
+    prepared_insert.execute([])?;
+    // prepared_insert.execute([rusqlite::named_params! {
+    //     ":cols": headers.join(","),
+    //     ":vals": row.iter().collect::<Vec<&str>>().join(",")
+    // }])?;
+
+    // A simple insert works, but the insert I want with world replacements does not.
+    // I think it's because I'm being too liberal about prepared statements parameter replacements.
 
     Ok(())
 }
